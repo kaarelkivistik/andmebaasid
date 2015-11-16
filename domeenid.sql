272e2d005873e57e6b6089c4e53b8ed18e71aa05
unknown <Kaarel.Kivistik1@ICT-40415K.intra.ttu.ee>
2015-11-16 14:55:12 +0200

    parandused füüsilises andmemudelis

--- domeenid.sql
+++ domeenid.sql
@@ -1,3 +1,4 @@
+DROP DOMAIN IF EXISTS d_nimetus;
 CREATE DOMAIN d_nimetus varchar(100) NOT NULL DEFAULT 'nimetu'
 	CHECK (VALUE !~ '^\s*$');
 	
@@ -5,8 +6,10 @@ ALTER TABLE garaazh.kauba_kategooria DROP CONSTRAINT IF EXISTS CHK_kauba_kategoo
 ALTER TABLE garaazh.kauba_staatus DROP CONSTRAINT IF EXISTS CHK_kauba_staatuse_nimetus_ei_ole_tyhi;
 ALTER TABLE garaazh.tootaja_staatus DROP CONSTRAINT IF EXISTS CHK_tootaja_staatuse_nimetus_ei_ole_tyhi;
 ALTER TABLE garaazh.tootaja_roll DROP CONSTRAINT IF EXISTS CHK_tootaja_rolli_nimetus_ei_ole_tyhi;
+ALTER TABLE garaazh.organisatsiooni_tyyp DROP CONSTRAINT IF EXISTS CHK_organisatsiooni_tyybi_nimetus_ei_ole_tyhi;
 
 ALTER TABLE garaazh.kauba_kategooria ALTER COLUMN nimetus TYPE d_nimetus;
 ALTER TABLE garaazh.kauba_staatus ALTER COLUMN nimetus TYPE d_nimetus;
 ALTER TABLE garaazh.tootaja_staatus ALTER COLUMN nimetus TYPE d_nimetus;
 ALTER TABLE garaazh.tootaja_roll ALTER COLUMN nimetus TYPE d_nimetus;
+ALTER TABLE garaazh.organisatsiooni_tyyp COLUMN organisatsiooni_tyybi_nimetus TYPE d_nimetus;
