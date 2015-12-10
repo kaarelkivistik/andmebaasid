d6eb41f2725fc88e6d550e7e966d325303a4606b
Kaarel Kivistik <pohlamoos@gmail.com>
2015-12-10 23:30:43 +0200

    paroolid hashimine

--- insert_laused.sql
+++ insert_laused.sql
@@ -5,11 +5,11 @@ INSERT INTO organisatsioon(organisatsiooni_kood, organisatsiooni_tyybi_kood, aad
 INSERT INTO organisatsioon(organisatsiooni_kood, organisatsiooni_tyybi_kood, aadress, e_mail, nimi, registri_kood, tarneaeg, hinne, riik) VALUES (5, 2, 'Viru tn 9, Tallinn 34283, Eesti', 'info@intel.com', 'Intel','510A2746B6G', 				5.0, 5.0, 'EE');
 INSERT INTO organisatsioon(organisatsiooni_kood, organisatsiooni_tyybi_kood, aadress, e_mail, nimi, registri_kood, tarneaeg, hinne, riik) VALUES (6, 1, 'Kopli liinid, Tallinn 01337, Eesti', 'mart@hot.ee', 'Mardi Kiirkuller','43J2HJHJH23', 	5.0, 5.0, 'EE');
 
-INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (1, 'hans@mets.ee', 'Hans', '38503053399', 'parool1', 'Mets');
-INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (2, 'jaan@koer.ee', 'Jaan', '38404053539', 'parool2', 'Koer');
-INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (3, 'nele@keel.ee', 'Nele', '49011124567', 'parool3', 'Keel');
-INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (4, 'ingrid@leht.ee', 'Ingrid', '48503058475', 'parool4', 'Leht');
-INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (5, 'jaanus@paber.ee', 'Jaanus', '38803084390', 'parool5', 'Paber');
+INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (1, 'hans@mets.ee', 'Hans', '38503053399', public.crypt('parool1', public.gen_salt('bf', 11)), 'Mets');
+INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (2, 'jaan@koer.ee', 'Jaan', '38404053539', public.crypt('parool2', public.gen_salt('bf', 11)), 'Koer');
+INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (3, 'nele@keel.ee', 'Nele', '49011124567', public.crypt('parool3', public.gen_salt('bf', 11)), 'Keel');
+INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (4, 'ingrid@leht.ee', 'Ingrid', '48503058475', public.crypt('parool4', public.gen_salt('bf', 11)), 'Leht');
+INSERT INTO tootaja(tootaja_kood, e_mail, eesnimi, isikukood, parool, perenimi) VALUES (5, 'jaanus@paber.ee', 'Jaanus', '38803084390', public.crypt('parool5', public.gen_salt('bf', 11)), 'Paber');
 
 INSERT INTO kaup(kauba_kood, hind, korgus, laius, nimetus, pikkus, kauba_kategooria, kauba_staatus, tootaja, tarnija, tootja) VALUES ('123456789', 15.5, 200, 200, 'DELL 90W AC Adapter', 			200, 1, 1, 1, 6, 1);
 INSERT INTO kaup(kauba_kood, hind, korgus, laius, nimetus, pikkus, kauba_kategooria, kauba_staatus, tootaja, tarnija, tootja) VALUES ('987654321', 20.5, 250, 250, 'Playstation 4', 				250, 7, 1, 1, 6, 2);
