10b0cb8d8cd455238ee4bbe6eed05ff9b3320d3d
Kaarel Kivistik <pohlamoos@gmail.com>
2015-12-11 00:10:52 +0200

    parandasin vaateid

--- vaated.sql
+++ vaated.sql
@@ -1,5 +1,5 @@
-DROP VIEW IF EXISTS garaazh.kauba_kuvatav_info;
-CREATE VIEW garaazh.kauba_kuvatav_info WITH (security_barrier) AS
+DROP VIEW IF EXISTS kauba_kuvatav_info;
+CREATE VIEW kauba_kuvatav_info WITH (security_barrier) AS
   SELECT
     kaup.nimetus                                                       AS kauba_nimi,
     kaup.hind,
@@ -11,24 +11,24 @@ CREATE VIEW garaazh.kauba_kuvatav_info WITH (security_barrier) AS
     tootja.hinne                                                       AS tootja_reiting,
     tarnija.nimi                                                       AS tarnija,
     tarnija.tarneaeg                                                   AS tarnija_tarneaeg
-  FROM garaazh.kaup AS kaup
-    INNER JOIN garaazh.organisatsioon AS tootja ON kaup.tootja = tootja.organisatsiooni_kood
-    INNER JOIN garaazh.kauba_kategooria AS kategooria ON kaup.kauba_kategooria = kategooria.kauba_kategooria_kood
-    INNER JOIN garaazh.organisatsioon AS tarnija ON kaup.tarnija = tarnija.organisatsiooni_kood
+  FROM kaup AS kaup
+    INNER JOIN organisatsioon AS tootja ON kaup.tootja = tootja.organisatsiooni_kood
+    INNER JOIN kauba_kategooria AS kategooria ON kaup.kauba_kategooria = kategooria.kauba_kategooria_kood
+    INNER JOIN organisatsioon AS tarnija ON kaup.tarnija = tarnija.organisatsiooni_kood
   WHERE kaup.kauba_staatus = 1;
 
-COMMENT ON VIEW garaazh.kauba_kuvatav_info IS 'Selles tabelis on kogu info, mida kasutaja peaks veebilehel nägema.';
+COMMENT ON VIEW kauba_kuvatav_info IS 'Selles tabelis on kogu info, mida kasutaja peaks veebilehel nägema.';
 
-DROP VIEW IF EXISTS garaazh.ylemkategooriad;
-CREATE VIEW garaazh.ylemkategooriad WITH (security_barrier) AS
+DROP VIEW IF EXISTS ylemkategooriad;
+CREATE VIEW ylemkategooriad WITH (security_barrier) AS
   SELECT
     kategooria.kauba_kategooria_kood AS kategooria_kood,
     kategooria.nimetus,
     kategooria.kirjeldus
-  FROM garaazh.kauba_kategooria AS kategooria
+  FROM kauba_kategooria AS kategooria
   WHERE kategooria.ylem_kategooria IS NULL;
 
-COMMENT ON VIEW garaazh.ylemkategooriad IS 'Selles tabelis on kõikide kaubaliikide ülemkategooriad';
+COMMENT ON VIEW ylemkategooriad IS 'Selles tabelis on kõikide kaubaliikide ülemkategooriad';
 
 
 CREATE VIEW aktiivne_tootaja WITH (security_barrier) AS
@@ -39,12 +39,12 @@ CREATE VIEW aktiivne_tootaja WITH (security_barrier) AS
          tootaja.e_mail AS e_mail,
          tootaja.eesnimi AS eesnimi,
          tootaja.perenimi AS perenimi
-  FROM garaazh.tootaja AS tootaja
+  FROM tootaja AS tootaja
   WHERE tootaja_staatuse_kood = 1;
 
 COMMENT ON VIEW aktiivne_tootaja IS 'Aktiivse töötaja vaatamine';
 
-CREATE VIEW tootaja_roll WITH (security_barrier) AS
+CREATE VIEW kaupade_haldaja WITH (security_barrier) AS
   SELECT tootaja.tootaja_kood AS tootaja_kood,
          tootaja.isikukood AS isikukood,
          tootaja.tootaja_rolli_kood AS tootaja_rolli_kood,
@@ -52,11 +52,7 @@ CREATE VIEW tootaja_roll WITH (security_barrier) AS
          tootaja.e_mail AS e_mail,
          tootaja.eesnimi AS eesnimi,
          tootaja.perenimi AS perenimi
-  FROM garaazh.tootaja AS tootaja
+  FROM tootaja AS tootaja
   WHERE tootaja_rolli_kood = 1;
 
-COMMENT ON VIEW tootaja_roll IS 'Kaupade haldajate vaatamine';
-
-
-
-
+COMMENT ON VIEW kaupade_haldaja IS 'Kaupade haldajate vaatamine';
