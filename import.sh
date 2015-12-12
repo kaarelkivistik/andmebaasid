4541976f1080eb57258fff5417cb6b31a61b0873
Kaarel Kivistik <pohlamoos@gmail.com>
2015-12-12 13:54:17 +0200

    .sql skriptide importimise skript

---
+++ import.sh
@@ -0,0 +1,10 @@
+DB = "t123665";
+
+psql $DB < autentimine.sql
+psql $DB < andmebaas.sql
+psql $DB < domeenid.sql
+psql $DB < funktsioonid.sql
+psql $DB < trigerid.sql
+psql $DB < vaated.sql
+psql $DB < klassifikaatorid.sql
+psql $DB < andmed.sql
