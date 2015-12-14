f40a3460963bc18c35a1421eaadf6fab527baecc
martlaus <martlaus1@gmail.com>
2015-12-14 15:18:55 +0200

    Update trigerid.sql

--- trigerid.sql
+++ trigerid.sql
@@ -5,9 +5,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+COMMENT ON FUNCTION kaupa_ei_loodud_aktiivsena(*) IS 'Kui kauba staatus pole loomisel aktiivne, siis visatakse selle funktsiooniga erand.';
+
+
 CREATE TRIGGER kaup_insert BEFORE INSERT ON kaup
   FOR EACH ROW WHEN (NEW.kauba_staatus != 1) EXECUTE PROCEDURE kaupa_ei_loodud_aktiivsena();
 
+COMMENT ON TRIGGER kaup_insert ON kaup IS 'Kauba loomisel kontrollitakse kas loodav kaup on aktiivne';
 
 CREATE OR REPLACE FUNCTION kaupa_yritati_kustutada()
   RETURNS TRIGGER AS $$
@@ -16,5 +20,10 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+COMMENT ON FUNCTION kaupa_yritati_kustutada(*) IS 'Kauba kustutamisel visatakse selle funktsiooniga erand.';
+
+
 CREATE TRIGGER kaup_delete BEFORE DELETE ON kaup
   FOR EACH ROW EXECUTE PROCEDURE kaupa_yritati_kustutada();
+
+COMMENT ON TRIGGER kaup_delete ON kaup IS 'Kauba kustutamist jälgitakse ning enne seda visatakse erand.';
