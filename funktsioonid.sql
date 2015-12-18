6aa846ef1ce4637ea6ac258d63e44984fd4358ae
Kaarel Kivistik <pohlamoos@gmail.com>
2015-12-18 15:03:34 +0200

    funktsioonide nimede update, kauba uuendamise funktsiooni fix (laius oli puudu)

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -1,16 +1,16 @@
-CREATE OR REPLACE FUNCTION uuenda_kaupa(uuendatava_kauba_kood VARCHAR(20), _nimetus VARCHAR(255), _hind NUMERIC(19), _pikkus INT, _korgus INT, laius INT, _pildi_aadress VARCHAR(255), _kauba_kategooria INT, _tootja INT, _tarnija INT)
+CREATE OR REPLACE FUNCTION f_uuenda_kaupa(uuendatava_kauba_kood VARCHAR(20), _nimetus VARCHAR(255), _hind NUMERIC(19), _pikkus INT, _korgus INT, _laius INT, _pildi_aadress VARCHAR(255), _kauba_kategooria INT, _tootja INT, _tarnija INT)
 
 RETURNS VOID AS $$
 
 UPDATE kaup as k
-SET nimetus = _nimetus, hind = _hind, pikkus = _pikkus, korgus = _korgus, pildi_aadress = _pildi_aadress, kauba_kategooria = _kauba_kategooria, tootja = _tootja, tarnija = _tarnija WHERE kauba_kood = uuendatava_kauba_kood AND kauba_staatus != 3;
+SET nimetus = _nimetus, hind = _hind, pikkus = _pikkus, korgus = _korgus, laius = _laius, pildi_aadress = _pildi_aadress, kauba_kategooria = _kauba_kategooria, tootja = _tootja, tarnija = _tarnija WHERE kauba_kood = uuendatava_kauba_kood AND kauba_staatus != 3;
 
 $$ LANGUAGE SQL SECURITY DEFINER SET SEARCH_PATH = public, pg_temp;
 
 COMMENT ON FUNCTION uuenda_kaupa(VARCHAR, VARCHAR, NUMERIC, INT, INT, INT, VARCHAR, INT, INT, INT) IS '(OP6.1) Muudab kauba infot.';
 
 
-CREATE OR REPLACE FUNCTION kustuta_kaup(kauba_kood VARCHAR) 
+CREATE OR REPLACE FUNCTION f_kustuta_kaup(kauba_kood VARCHAR) 
 
 RETURNS void AS $$
 
@@ -23,7 +23,7 @@ SET search_path = public, pg_temp;
 COMMENT ON FUNCTION kustuta_kaup(kauba_kood VARCHAR) IS '(OP7.1) Kauba kustutamisel muutub kauba staatus 0-ks.';
 
 
-CREATE OR REPLACE FUNCTION uuenda_kauba_staatus_aktiivseks(uuendatava_kauba_kood VARCHAR(20))
+CREATE OR REPLACE FUNCTION f_uuenda_kauba_staatus_aktiivseks(uuendatava_kauba_kood VARCHAR(20))
 
 RETURNS VOID AS $$
 
@@ -36,7 +36,7 @@ $$ LANGUAGE SQL SECURITY DEFINER SET SEARCH_PATH = public, pg_temp;
 COMMENT ON FUNCTION uuenda_kauba_staatus_aktiivseks (VARCHAR) IS '(OP3.1) Seab kauba aktiivseks.';
 
 
-CREATE OR REPLACE FUNCTION uuenda_kauba_staatus_mitteaktiivseks(uuendatava_kauba_kood VARCHAR(20))
+CREATE OR REPLACE FUNCTION f_uuenda_kauba_staatus_mitteaktiivseks(uuendatava_kauba_kood VARCHAR(20))
 
 RETURNS VOID AS $$
 
